13ba1a07b1b829dd06814f01f0df47d61052687b
Eknoor Singh <91742955+Eknoorsinghkohli@users.noreply.github.com>
2025-01-27 10:17:05 -0700

    week 2 assignment commit

--- app/page.js
+++ app/page.js
@@ -3,8 +3,7 @@ import Link from "next/link";
 export default function page(){
   return(
     <div>
-      <h1>Hello, World</h1>
-      <p>This is a simple web page.</p>
+      <h1> CPRG 306: Web Development 2 - Assignments</h1>
       <p>
         Link to week 2: <Link href="week-2">Week 2</Link>
       </p>
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+export default function StudentInfo() {
+    return (
+      <main>
+        <p>Eknoor singh kohli</p>
+        <p>Link to Github: 
+        <a href="https://github.com/SinghKohliEknoor/cprg306-assignments.git"> My Github </a>    
+        </p>    
+      </main>
+    );
+  }
