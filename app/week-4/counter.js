4135bea0f0667663a687b84a60db8e8c269a29a7
Eknoor Singh <91742955+Eknoorsinghkohli@users.noreply.github.com>
2025-02-10 19:03:21 -0700

    week 4 final

--- app/week-4/counter.js
+++ app/week-4/counter.js
@@ -6,16 +6,25 @@ export default function Counter(){
 
     const increment = () => {
         setCount(count +1);
-        if(count==10){
-            alert("Value 10 achieved");
-        }
+    }
+    const decrement = () => {
+        setCount(count -1);
     }
 
     return(
-        <div>
-            <p>{count}</p>
-            <button onClick={increment} type="button"
-            className="p-2 m-2 bg-blue-700 hover:bg-blue-500  active:bg-red-200 disabled:bg-gray-300"  disabled={count===10}>increment</button>
+        <div className="flex items-center justify-center">
+            <div className="bg-green-500 rounded-2xl p-4 m-4">
+                <div className="text-4xl font-bold flex  items-center justify-center ">
+                    <h1>{count}</h1>
+                </div>
+                <div >
+                    <button onClick={increment} type="button"
+                    className="p-2 m-2 border-solid border-2 border-black rounded-full bg-blue-700 hover:bg-blue-500  active:bg-red-200 disabled:bg-gray-300"  disabled={count===20}>Increment</button>
+                
+                    <button onClick={decrement} type="button"
+                    className="p-2 m-2 bg-blue-700 border-solid border-black border-2 rounded-full hover:bg-blue-500  active:bg-red-200 disabled:bg-gray-300"  disabled={count===0}>Decrement</button>
+                </div>
+            </div>
         </div>
     )
 }
