13c92de6a740ed357aa3b6ac0e24fde72ef6e245
Eknoor Singh <91742955+Eknoorsinghkohli@users.noreply.github.com>
2025-03-04 00:59:34 -0700

    week-6 button

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -19,11 +19,12 @@ export default function ItemList(){
             <div className="flex">
             <h2 className="text-white font-bold text-[20px] ">Sort By:</h2>
             <button className={`{px-4 py-2 ml-4 rounded-lg font-medium transition-colors duration-300 ${
-                        sortType === "category" ? "bg-blue-500 text-white" : "bg-gray-400 text-black"}`
-                    } onClick={()=>setSortType("category")}>Category</button>
-            <button className={`{px-4 py-2 ml-4 rounded-lg font-medium transition-colors duration-300 ${
                         sortType === "name" ? "bg-blue-500 text-white" : "bg-gray-400 text-black"}`
                     } onClick={()=>setSortType("name")} >Name</button>
+            <button className={`{px-4 py-2 ml-4 rounded-lg font-medium transition-colors duration-300 ${
+                        sortType === "category" ? "bg-blue-500 text-white" : "bg-gray-400 text-black"}`
+                    } onClick={()=>setSortType("category")}>Category</button>
+            
             </div>
             <div>
             {sortedItems.map((item) => (
